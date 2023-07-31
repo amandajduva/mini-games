7c65f91a7db9870a19b9734da72805d3fdedc7ee
Amanda Duva <amanda.j.duva@gmail.com>
2023-07-31 17:01:33 -0400

    added home routes and login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,9 +3,19 @@ const router = require("express").Router();
 // const withAuth = require("../utils/auth");
 
 router.get("/gameOne", async (req, res) => {
+    try {
+        res.render("gameOne");
+    } catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get("/", async (req, res) => {
 	try {
-    res.render("gameOne");
-	} catch (err) {
+		res.render("login-signup", {
+			logged_in: req.session.logged_in
+		});
+	} catch (error) {
 		res.status(500).json(err);
 	}
 });
