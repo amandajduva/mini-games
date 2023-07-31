5fb1a194a1acd42b2b81b80a82d4a6a4ea6189e0
casignacio <carloanthonyignacio@gmail.com>
2023-07-31 16:28:51 -0600

    updated models folder

--- models/index.js
+++ models/index.js
@@ -1,1 +1,18 @@
-// 
+const User = require("./user");
+const Highscore = require("./highscore");
+const Games = require("./games");
+
+User.hasMany(Highscore, {
+    foreignKey: "id",
+    onDelete: "CASCADE"
+});
+
+Highscore.belongsTo(User, {
+    foreignKey: "id"
+});
+
+Games.belongsTo(User, {
+    foreignKey: "id"
+});
+
+module.exports = { User, Highscore, Games };
