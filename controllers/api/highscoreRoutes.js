1f0c28c5db692910b8b6f57ab660056036ea0f51
Amanda Duva <amanda.j.duva@gmail.com>
2023-07-31 21:58:04 -0400

    uncommented highscores

--- controllers/api/highscoreRoutes.js
+++ controllers/api/highscoreRoutes.js
@@ -1,82 +1,82 @@
-// const router = require("express").Router();
-// const { Highscore, User } = require("../../models");
+const router = require("express").Router();
+const { Highscore, User } = require("../../models");
 
-// router
-//     .post("/save", (req, res) => {
-//         Highscore.create({
-//             highscore: req.body.highscore,
-//             gamename: req.body.gamename,
-//             userId: req.session.userId,
-//         }).then((dbScoreData) => {
-//             res.json(dbScoreData);
-//         });
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
+router
+    .post("/save", (req, res) => {
+        Highscore.create({
+            highscore: req.body.highscore,
+            gamename: req.body.gamename,
+            userId: req.session.userId,
+        }).then((dbScoreData) => {
+            res.json(dbScoreData);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 
-// router.get("/gamescores/:gamename", (req, res) => {
-//     Highscore.findAll({
-//         where: {
-//             gamename: req.params.gamename
-//         },
-//         attributes: [
-//             "highscore",
-//             "gamename",
-//             "createdAt"
-//         ],
-//         order: [["highscore", "DESC"]],
-//         include: [{
-//             model: User,
-//             attributes: ["gamertag"]
-//         },
-//         ]
-//     })
-//         .then(dbScoreData => {
-//             if (!dbScoreData) {
-//                 res.status(404).json({ message: "No scores for this game" });
-//                 return;
-//             }
-//             res.json(dbScoreData);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
+router.get("/gamescores/:gamename", (req, res) => {
+    Highscore.findAll({
+        where: {
+            gamename: req.params.gamename
+        },
+        attributes: [
+            "highscore",
+            "gamename",
+            "createdAt"
+        ],
+        order: [["highscore", "DESC"]],
+        include: [{
+            model: User,
+            attributes: ["gamertag"]
+        },
+        ]
+    })
+        .then(dbScoreData => {
+            if (!dbScoreData) {
+                res.status(404).json({ message: "No scores for this game" });
+                return;
+            }
+            res.json(dbScoreData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
-// router.get("/myscores/:gamename", (req, res) => {
-//     Highscore.findAll({
-//         where: {
-//             gamename: req.params.gamename
-//         },
-//         attributes: [
-//             "highscore",
-//             "gamename",
-//             "createdAt"
-//         ],
-//         order: [["highscore", "DESC"]],
-//         include: [{
-//             model: User,
-//             where: {
-//                 id: req.session.userId
-//             },
-//             attributes: ["gamertag"]
-//         },
-//         ]
-//     })
-//         .then(dbScoreData => {
-//             if (!dbScoreData) {
-//                 res.status(404).json({ message: "No scores for this game" });
-//                 return;
-//             }
-//             res.json(dbScoreData);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
+router.get("/myscores/:gamename", (req, res) => {
+    Highscore.findAll({
+        where: {
+            gamename: req.params.gamename
+        },
+        attributes: [
+            "highscore",
+            "gamename",
+            "createdAt"
+        ],
+        order: [["highscore", "DESC"]],
+        include: [{
+            model: User,
+            where: {
+                id: req.session.userId
+            },
+            attributes: ["gamertag"]
+        },
+        ]
+    })
+        .then(dbScoreData => {
+            if (!dbScoreData) {
+                res.status(404).json({ message: "No scores for this game" });
+                return;
+            }
+            res.json(dbScoreData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
-// module.exports = router;
+module.exports = router;
